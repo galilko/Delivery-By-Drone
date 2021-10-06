896649e43ad6f592913aa532ef6c5b366a041364
galilko <ggabay@g.jct.ac.il>
2021-10-06 14:03:24 +0300

    Inital code + Welcome functions

---
+++ Targil0/Program6024.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6024();
+            Welcome2498();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome2498();
+        private static void Welcome6024()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
