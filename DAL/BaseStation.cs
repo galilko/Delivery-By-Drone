e5c1a5dd8686c516d7702208258ddf11c788f1a0
galilko <ggabay@g.jct.ac.il>
2021-10-13 17:43:03 +0300

    up

---
+++ DAL/BaseStation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        struct BaseStation
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+            public int ChargeSlots { get; set; }
+            public override string ToString()
+            {
+                string result = "";
+                result += $"Id is: {Id} \n ";
+                return result;
+            }
+        }
+    }
+}
