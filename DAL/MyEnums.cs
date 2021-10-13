4d225070ba303da83856161d4747e0d15d8bc2f0
galilko <ggabay@g.jct.ac.il>
2021-10-13 22:57:25 +0300

    up

---
+++ DAL/MyEnums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        enum WeightCategories
+        {
+            light,
+            medium,
+            heavy
+        }
+        enum DroneStatusCategories
+        {
+            free,
+            maintenance,
+            delivery
+        }
+        enum Priority
+        {
+            normal,
+            fast,
+            emergency
+        }
+        class MyEnums
+        {
+        }
+    }
+}
